8f625b0c08055359356930c36a1a1521bbc52e39
Luc cole <luccoleb@yahoo.com>
2021-08-08 20:04:20 -0400

    removed useEffectfrom Posts.js as it was unused

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import PostSnippet from './PostSnippet';
 import { Box, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
